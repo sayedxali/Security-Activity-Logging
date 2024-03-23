0b84783020c6e377195c078bcf07405efc0dfaef
SayedAli <sayedxali7@gmail.com>
2024-03-23 23:59:48 +0330

    This is the first commit.

---
+++ src/main/java/com/seyed/ali/LoggingLogbackJwtSecurityAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.seyed.ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingLogbackJwtSecurityAuditingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingLogbackJwtSecurityAuditingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seyed/ali/LoggingLogbackJwtSecurityAuditingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyed.ali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingLogbackJwtSecurityAuditingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
